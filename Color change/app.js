acf1d3b538d8b964fb87a02f0b3bfe31d8a19f07
Patryk Golab <patrykus64@gmail.com>
2022-08-12 16:29:43 +0200

    Second app

--- Color change/app.js
+++ Color change/app.js
@@ -1,3 +1,13 @@
-btn.addEventListener("click",function(){
-    console.log("test");
+const color = ["green","blue","red","white"];
+const button = document.getElementById("btn");
+const color_picker = document.querySelector(".color")
+
+btn.addEventListener("click",function() {
+    const number =randomNumber();
+    
+    document.body.style.backgroundColor = color[number];
+    color_picker.textContent = color[number]
 });
+const randomNumber = () =>{
+ return Math.floor(Math.random()*color.length)
+}
